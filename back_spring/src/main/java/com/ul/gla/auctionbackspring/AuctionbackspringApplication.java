8fb98acda0da0557c0afe69c0de552957c9e8a92
Matthias <matt.bert@live.fr>
2020-11-16 17:38:31 +0100

    Initial commit

---
+++ back_spring/src/main/java/com/ul/gla/auctionbackspring/AuctionbackspringApplication.java
@@ -0,0 +1,13 @@
+package com.ul.gla.auctionbackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionbackspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionbackspringApplication.class, args);
+    }
+
+}
---
+++ back_spring/src/test/java/com/ul/gla/auctionbackspring/AuctionbackspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ul.gla.auctionbackspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionbackspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
