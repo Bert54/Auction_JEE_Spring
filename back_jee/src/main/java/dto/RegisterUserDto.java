6df1d926885441daaea774bf7e9449cb599b0d71
Damien LEVY <damien.levy4@etu.univ-lorraine.fr>
2021-01-20 12:14:30 +0100

    ajout de champ dans le back jee user

--- back_jee/src/main/java/dto/RegisterUserDto.java
+++ back_jee/src/main/java/dto/RegisterUserDto.java
@@ -4,6 +4,60 @@ public class RegisterUserDto {
 
     String username;
     String password;
+    String firstName;
+    String lastName;
+    String street;
+    String city;
+    int postcode;
+    int houseNumber;
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
     public String getUsername() {
         return username;
--- back_jee/src/main/java/entities/User.java
+++ back_jee/src/main/java/entities/User.java
@@ -16,11 +16,30 @@ public class User implements Serializable {
 
     private String password;
 
+    private String firstName;
+
+    private String lastName;
+
+    private String street;
+
+    private String city;
+
+    private int postcode;
+
+    private int houseNumber;
+
     public User() {}
 
-    public User(String username, String password) {
+    public User(String username, String password, String firstName, String lastName, String street, String city, int postcode, int houseNumber) {
         this.username = username;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.street = street;
+        this.city = city;
+        this. postcode = postcode;
+        this.houseNumber = houseNumber;
+
     }
 
     public long getId() {
