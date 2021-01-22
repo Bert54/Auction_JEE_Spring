713647383fa7b2085eb58bdb9d0f212e876d48a7
Damien LEVY <damien.levy4@etu.univ-lorraine.fr>
2021-01-22 13:05:55 +0100

    resolution bug frint

--- front/src/app/shared/validators/AdressValidator.ts
+++ front/src/app/shared/validators/AdressValidator.ts
@@ -3,8 +3,8 @@ import {AbstractControl, ValidationErrors} from '@angular/forms';
 export class AdressValidator {
 
   static adressComplete(c: AbstractControl): ValidationErrors  | null {
-    if (c.get('street').value !== '' || c.get('city').value !== '' || c.get('postcode').value !== 0) {
-      if (c.get('street').value === '' || c.get('city').value === '' || c.get('postcode').value === 0) {
+    if (c.get('street').value !== '' || c.get('city').value !== '' || c.get('postcode').value !== '') {
+      if (c.get('street').value === '' || c.get('city').value === '' || c.get('postcode').value === '') {
       return { empty: true };
       }
     }
