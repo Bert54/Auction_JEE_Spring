1e254baa5d4a18b16747ec624b2b3caf9dcab024
robocopv6 <redsirop@gmail.com>
2021-01-23 16:50:04 +0100

    scheduled offer work

--- back_spring/src/main/java/com/ul/gla/auctionbackspring/AuctionbackspringApplication.java
+++ back_spring/src/main/java/com/ul/gla/auctionbackspring/AuctionbackspringApplication.java
@@ -2,8 +2,10 @@ package com.ul.gla.auctionbackspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AuctionbackspringApplication {
 
     public static void main(String[] args) {
--- back_spring/src/main/java/com/ul/gla/auctionbackspring/dao/OfferRepository.java
+++ back_spring/src/main/java/com/ul/gla/auctionbackspring/dao/OfferRepository.java
@@ -1,15 +1,21 @@
 package com.ul.gla.auctionbackspring.dao;
 
 import com.ul.gla.auctionbackspring.entities.Offer;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OfferRepository {
+import javax.transaction.Transactional;
+
+public interface OfferRepository extends CrudRepository<Offer, Integer> {
 
     @Query("SELECT o FROM Offer AS o")
     public Offer find();
 
     public Offer save(Offer offer);
 
+    @Transactional
+    @Modifying
     @Query("DELETE FROM Offer o")
     public int delete();
 
