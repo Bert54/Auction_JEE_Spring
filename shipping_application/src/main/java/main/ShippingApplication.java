346190b5d1ccc413296771cda7e0ac8bf8f57862
Matthias <matt.bert@live.fr>
2021-01-24 15:46:27 +0100

    improved RMQ communication

--- shipping_application/src/main/java/main/ShippingApplication.java
+++ shipping_application/src/main/java/main/ShippingApplication.java
@@ -14,6 +14,7 @@ public class ShippingApplication {
     private static Channel channel;
 
     private final static String QUEUE_NAME_SEND = "OrderQueueSend";
+    private final static String QUEUE_NAME_SEND_CONFIRMATION = "OrderQueueConfirmation";
     private final static String QUEUE_NAME_RECEIVE = "OrderQueueReceive";
 
     public static void main(String[] args) throws IOException, TimeoutException {
@@ -28,9 +29,12 @@ public class ShippingApplication {
         channel = connection.createChannel();
         channel.queueDeclare(QUEUE_NAME_SEND, false, false, false, null);
         channel.queueDeclare(QUEUE_NAME_RECEIVE, false, false, false, null);
+        channel.queueDeclare(QUEUE_NAME_SEND_CONFIRMATION, false, false, false, null);
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received order '" + message + "' from auctions application.");
+            channel.basicPublish("", QUEUE_NAME_SEND_CONFIRMATION, null, message.getBytes());
+            System.out.println(" [x] Confirmation for order '" + message + "' sent to auctions application.");
         };
         channel.basicConsume(QUEUE_NAME_SEND, true, deliverCallback, consumerTag -> { });
         mainLoop();
